09cc54beca4d03a02045ac91315e7d01679bdec5
mostafa <mostafa.osman.fathi@gmail.com>
2025-10-10 15:45:06 +0300

    fixing function format

--- pyobjtools/objfile.py
+++ pyobjtools/objfile.py
@@ -14,9 +14,9 @@ class ObjFile:
             self.obj_file_path = obj_file_path
             self.meta_data, self.obj_data = csv_utils.read_cv2_to_obj(self.obj_file_path)
 
-    def convert_to_csv(self, output_path='../outputs'):
+    def convert_to_csv(self, output_path='./outputs'):
         file_name = os.path.splitext(os.path.basename(self.obj_file_path))[0]
-        csv_utils.write_object_to_csv(self.obj_data, self.obj_file_path, file_name, output_path)
+        csv_utils.write_object_to_csv(self.meta_data, self.obj_data, file_name, output_path)
 
     def write_obj_file(self, write_path):
         io_utils.write_obj(self.meta_data, self.obj_data, write_path)
