a6f0ec7a52616a6769074dcf0cd7ef41ae1d202f
mostafa <mostafa.osman.fathi@gmail.com>
2025-10-10 22:14:32 +0300

    adding demo of how to use

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
+import csv
+import os
+
 from pyobjtools import ObjFile
 
+def main():
+    obj = ObjFile('samples/texturedMesh.obj')  # load .obj file
+
+    removeList = set()
+    for file in os.listdir('samples/texturedMeshNoise/clusters'):
+        filePath = os.path.join('samples/texturedMeshNoise/clusters', file)
+        for line in csv.reader(open(filePath)):
+            removeList.add(int(line[0]))
+    print(len(removeList))
+
+    obj.remove_vertices_list_by_ids(removeList)
+    obj.write_obj_file('removed_texturedMesh.obj')
+
+
+def mainCube():
+    obj = ObjFile('samples/cube.obj')
+    removeList = [2,3]
+    obj.remove_vertices_list_by_ids(removeList)
+    obj.write_obj_file('removed_cube.obj')
+
+
 if __name__ == '__main__':
-    obj = ObjFile('samples/cube.obj')  # load .obj file
-    # obj = ObjFile('outputs/cube')  # load from csv folder format
-
-    # list of vertices to remove
-    obj.remove_vertices_list_by_ids([2])
-
-    obj.write_obj_file('removed_cube.obj')  # save as .obj file
-
-    # obj.convert_to_csv('removed_cube')  # convert to csv folder format
+    main()
