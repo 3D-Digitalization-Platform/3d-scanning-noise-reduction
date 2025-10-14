813928445a5913175e20d58cda44fff35bb401ce
mostafa <mostafa.osman.fathi@gmail.com>
2025-10-14 22:17:51 +0300

    implemented cli for denoise

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
-import csv
 import os
+import sys
 
-from pyobjtools import ObjFile
-
-def main():
-    obj = ObjFile('samples/texturedMesh.obj')  # load .obj file
-
-    removeList = set()
-    for file in os.listdir('samples/texturedMeshNoise/clusters'):
-        filePath = os.path.join('samples/texturedMeshNoise/clusters', file)
-        for line in csv.reader(open(filePath)):
-            removeList.add(int(line[0]))
-    print(len(removeList))
-
-    obj.remove_vertices_list_by_ids(removeList)
-    obj.write_obj_file('removed_texturedMesh.obj')
+from noisereduction.HdbscanDenoise import HdbscanDenoise
+from noisereduction.RansackDenoise import RansackDenoise
 
 
-def mainCube():
-    obj = ObjFile('samples/cube.obj')
-    removeList = [2,3]
-    obj.remove_vertices_list_by_ids(removeList)
-    obj.write_obj_file('removed_cube.obj')
+def main(obj_path: str):
+    hd1Res = HdbscanDenoise(obj_path).run_hdbscan()
+    ransack1 = RansackDenoise(hd1Res).run_ransack()
+    hd2Res = HdbscanDenoise(ransack1).run_hdbscan()
+    print(f'your final denoised file in {hd2Res}')
 
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) != 2:
+        print("Usage: python main.py <path_to_obj_file>")
+        sys.exit(1)
+
+    obj_path = sys.argv[1]
+
+    if not obj_path.endswith(".obj"):
+        print("Must be a .obj file")
+        sys.exit(1)
+
+    if not os.path.exists(obj_path):
+        print(f"Error: File '{obj_path}' does not exist.")
+        sys.exit(1)
+
+    main(obj_path)
